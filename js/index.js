d96955974399674f5c6a7ffdea97133332e2f4d8
Amin Ansari <devansari@yahoo.com>
2022-04-18 16:18:54 +0430

    Function expressio transfored to function decleration

--- js/index.js
+++ js/index.js
@@ -2,20 +2,22 @@ const buttons = document.querySelectorAll("button");
 let pElement = document.querySelector("p");
 let letters = [];
 
-document.addEventListener("keypress", function (event) {
+function keyPush() {
+  pElement.innerHTML += event.key;
   for (var i = 0; i < buttons.length; i++) {
     if (buttons[i].innerHTML.toLowerCase() == event.key) {
       buttons[i].classList.add("keydown");
-      pElement.innerHTML += event.key;
     } else {
     }
   }
-});
-document.addEventListener("keyup", function (event) {
+}
+function keyLeave(event) {
   for (var i = 0; i < buttons.length; i++) {
     if (buttons[i].innerHTML.toLowerCase() == event.key) {
       buttons[i].classList.remove("keydown");
     } else {
     }
   }
-});
+}
+document.addEventListener("keypress", keyPush);
+document.addEventListener("keyup", keyLeave);
